b566963c327271ed648f4b04d6948c5ca06abe6a
bitorbit <89086375+tveecf@users.noreply.github.com>
2022-07-26 01:42:20 +0200

    Add files via upload

---
+++ uservar.py
@@ -0,0 +1,11 @@
+import xbmcaddon
+addon_id = xbmcaddon.Addon().getAddonInfo('id')
+
+'''#####-----Build File-----#####'''
+buildfile = 'https://tveecf.github.io/program/text/builds.xml'
+
+'''####tveecf.####-----Notificationstveecf.g####-----Notificationstveecf.gi####-----Notificationstveecf.git####-----Notificationstveecf.gith####-----Notificationstveecf.githu####-----Notificationstveecf.github####-----Notificationstveecf.github.####-----Notificationstveecf.github.i####-----Notifications/tveecf.github.i/####-----Notifications#-----Notifications File-----#####'''
+notify_url  = 'https://tveecf.github.io/program/text/notify.txt'
+
+'''#####-----Excludes-----#####'''
+excludes  = [addon_id, 'packages', 'Addons33.db', 'kodi.log', 'script.module.certifi', 'script.module.chardet', 'script.module.idna', 'script.module.requests', 'script.module.urllib3', 'backups', 'plugin.video.stubevavoo', 'repository.bitorbit']
